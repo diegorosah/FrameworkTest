85d28ade77436a106b80b3d42f7456741cc975d6
diegorosah <diegorosah.hc@gmail.com>
2020-10-28 22:08:54 -0300

    Add files via upload

---
+++ Metodos.java
@@ -0,0 +1,15 @@
+package br.com.framework.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class Metodos {
+
+	WebDriver driver;
+
+	public void navegador(String url, String navegador) {
+		
+		ASDASASDASDASD
+		
+	}
+
+}
